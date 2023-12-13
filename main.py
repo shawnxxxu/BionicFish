6681a41e92add25c5d09ef1147d52a9c16bc0e9e
SHAWNXU <xnx82661234@gmail.com>
2023-12-12 23:26:49 -0500

    final version

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from myfish import *
 
 if __name__ == '__main__':
     # Set up the time to complete game (unit: seconds)
-    time = 10
+    time = 30
     # Set up the speed of fish
-    speed = 500
+    speed = 200
 
     # Start the game
     run(time_count=time, fish_speed=speed)
