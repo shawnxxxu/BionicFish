d12abb96961d091709211d49ee92df19f0164940
SHAWNXU <xnx82661234@gmail.com>
2023-12-12 21:03:30 -0500

    1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # Set up the time to complete game (unit: seconds)
     time = 300
     # Set up the speed of fish
-    speed = 200
+    speed = 500
 
     # Start the game
     run(time_count=time, fish_speed=speed)
